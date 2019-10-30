c5ac1481fede228343cd2bf8f0c350099bd77cbe
dchinga <dchinga@outlook.com>
2019-10-30 11:33:24 -0500

    ~ cambio remoto

--- src/main/java/pe/edu/idat/daa2/app/App.java
+++ src/main/java/pe/edu/idat/daa2/app/App.java
@@ -2,6 +2,6 @@ package pe.edu.idat.daa2.app;
 
 public class App {
     public static void main(String[] args){
-        System.out.println("HOLA GITHUB");
+        System.out.println("HOLA GITHUB - obteniendo desde remoto");
     }
 }
